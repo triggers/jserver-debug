db8c2396f5499131b95c5a72ac2635b3c61428bd
Richard Potter <potter@axsh.net>
2017-03-14 18:11:21 +0900

    bin/printfify-defs.sh is now demoable

--- bin/printfify-defs.sh
+++ bin/printfify-defs.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 source "$(dirname $(readlink -f "$0"))/bashsteps-bash-utils-jan2017.source" || exit
 
 sourcefile="$1"
@@ -13,8 +15,31 @@ for ((ccc=1 ; ccc<100 ; ccc++)); do
     fi
 done
 
+leading_whitespace()
+{
+    read nonwhite therest <<<"$1"
+    printf -- "%s" "${1%%"$nonwhite"*}"
+}
+
 while IFS= read -r ln; do
-    case "$ln" in
-	*) printf -- "%s\n" "$ln"
+    w_one="$(leading_whitespace "$ln")"
+    therest_one="${ln#"$w_one"}"
+    case "$therest_one" in
+	*os.system*hhh*) : ;;
+	def\ *:*)
+	    printf -- "%s%s\n" "$w_one" "$therest_one"
+	    while IFS= read -r lineafter; do
+		[[ "$lineafter" == *os.system*hhh* ]] || break
+	    done
+	    w_two="$(leading_whitespace "$lineafter")"
+	    therest_two="${lineafter#"$w_two"}"
+	    printf -- "%sos.system(\"echo ,%s, >>~/hhh\")\n" "$w_two" "${therest_one%%(*}"
+	    printf -- "%s%s\n" "$w_two" "$therest_two"
+	    ;;
+	*)
+	    printf -- "%s\n" "$ln"
+	    ;;
     esac
 done <"$sourcefile"  >"$sourcefile.tmp"
+
+mv -f "$sourcefile.tmp" "$sourcefile"
